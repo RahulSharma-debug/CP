94d03ad8c71ac80c063b5e753942eea551eecfb0
RahulSharma-debug <rahulsharma9april@gmail.com>
2025-08-19 10:28:46 +0530

    Delete CP/LOOP/Q4.py

--- CP/LOOP/Q4.py
+++
@@ -1,8 +0,0 @@
-#reverse of an number
-num = int(input("Enter a number: "))
-reverse = 0
-while num > 0:
-    digit = num % 10
-    rev = rev * 10 + digit
-    num = num // 10
-print("Reverse of the number:", reverse)
